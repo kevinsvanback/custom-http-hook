e5ba69ed28635ea109f6ef49d3b98ffc5639bc6a
Kevin <kevin.svanback@gmail.com>
2022-02-04 11:48:16 +0100

    NewTask component

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -12,7 +12,7 @@ const useHttp = () => {
         requestConfig.url, {
         method: requestConfig.method ? requestConfig.method : 'GET',
         headers: requestConfig.headers ? requestConfig.headers : {},
-        body: JSON.stringify(requestConfig.body ? requestConfig.body : null),
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
